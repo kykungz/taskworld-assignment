717da8e92ecb836a1b39a322f737488ef907096f
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2019-07-03 19:27:22 +0700

    :tada: Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const App = () => (
+  <div className="App">
+    <div className="App-Header">
+      Demo ReactJS with Parceljs
+    </div>
+  </div>
+)
+
+export default App
